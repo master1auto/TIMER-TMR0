bbe7b98b9ae87f5b5d065d31bd83b58aa00809fe
trchemse-edu <trchemseddine@gmail.com>
2015-12-29 12:39:43 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,3 +1,20 @@
+unsigned cnt;                
+void interrupt() {
+    cnt++;                   
+    TMR0 = 177;                
+    INTCON = 0x20;            
 void main() {
-
+    OPTION_REG = 0x84;        
+    TRISB = 0;                
+    PORTB = 0x0;              
+    TMR0 = 177;                
+    INTCON = 0xA0;            
+    cnt = 0;                
+    do {                      
+        if (cnt == 100) {     
+            PORTB = PORTB++;  
+            cnt = 0;          
+        }
+    } while(1);
+}
 }
